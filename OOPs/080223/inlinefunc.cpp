d61115d41307ced9b155b17d7e91b42676ce97e9
Saransh Baniyal <saranshbaniyal14@gmail.com>
2023-02-08 11:42:56 +0530

    080223 minor fix in OOP inlinefunc.cpp

--- OOPs/080223/inlinefunc.cpp
+++ OOPs/080223/inlinefunc.cpp
@@ -1,13 +1,12 @@
 #include<iostream>
 using namespace std;
 
-inline int add(int a, int b){
-    return a+b;
+inline void add(int a, int b){
+    cout << "Sum: " << a+b << endl;
 }
 int main(){
     int a,b;
     cout << "Input values of a and b: ";
     cin >> a >> b;
-    int c = add(a,b);
-    cout << "Sum: " << c;
+    add(a,b);
 }
